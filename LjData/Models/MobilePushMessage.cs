04a3e16829a7168e482e5a5be1fff76d23077ea0
martin <zhengxi.li.19@neoma-bs.com>
2020-02-02 14:37:47 +0100

    建立MobilePushMessage

---
+++ LjData/Models/MobilePushMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LjData.Models
+{
+    [Table("Mobile_PushMessage")]
+    public class MobilePushMessage
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string UserGroup { get; set; }
+    }
+}
