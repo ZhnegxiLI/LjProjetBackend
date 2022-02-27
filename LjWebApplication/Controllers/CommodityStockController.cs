379de1ed552179f844a17bdd54f95963dd4fadb6
Zhengxi LI <zhengxi.li.19@neoma-bs.com>
2022-02-27 17:24:12 +0100

    DEV: add pagination

--- LjWebApplication/Controllers/CommodityStockController.cs
+++ LjWebApplication/Controllers/CommodityStockController.cs
@@ -3,11 +3,12 @@ using LjDataAccess.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LjWebApplication.Controllers
 {
-    [Route("api/CommodityStock")]
+    [Route("api/commodity-stock")]
     [ApiController]
     public class CommodityStockController : ControllerBase
     {
@@ -19,9 +20,19 @@ namespace LjWebApplication.Controllers
 
    
         [HttpGet]
-        public async Task<IEnumerable<CommodityStockDto>> GetData(string CommodityTextSearch, string ClientTextSearch, string CommodityType)
+        public async Task<ActionResult> GetData(string CommodityTextSearch, string ClientTextSearch, string CommodityType, int? Start, int? Limit)
         {
-            return await _commodityStockRepository.GetCommodityStocks(CommodityTextSearch, ClientTextSearch, CommodityType);
+            var result = await _commodityStockRepository.GetCommodityStocks(CommodityTextSearch, ClientTextSearch, CommodityType);
+            var totalAmount = result.Count();
+            if (Start!=null && Limit != null)
+            {
+                result = result.Skip((int)Start * (int)Limit).Take((int)Limit).ToList();
+            }
+            return Ok(new
+            {
+                TotalAmount = totalAmount,
+                Data = result
+            });
         }
     }
 }
